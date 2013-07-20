de6c82992f8907729be28e0be1a595c31fee5d22
Daniel <dan.john.roberts@gmail.com>
2013-07-20 19:58:29 +0100

    Minor Updates and Amends

--- application/controllers/courses.php
+++ application/controllers/courses.php
@@ -1,259 +1,232 @@
 <?php
-class courses extends CI_Controller {
-	
-
-
-	public function __construct()
-	{
-		parent::__construct();
-		$this->load->model('courses_model');
-		
-		$this->load->model('pages_model');
-		
-		$this->load->helper('url');
-	}
-
-	public function index()
-	{
-		$data['courses'] = $this->courses_model->course_type();
-		
-			
-		
-		$data['title'] = 'Full Course Archive';
-		
-
-		$this->load->view('templates/header', $data);
-	
-	 	$this->load->view('templates/nav-bar', $data);
-	
-	
-	$this->load->view('courses/index', $data);
-	$this->load->view('templates/footer');
-	
-	}
-	
-		/////////////////////////////////////////////////////////////////////////////////////////////////////////
-	
-	
-	public function course_type($slug)  /////////////////// Young / Adult / Higher / International
-	{
-		
-		 $data['get_page_catagories'] = $this->pages_model->get_page_catagories();
-	 
-	 
-	 	$data['page_list'] = $this->pages_model->get_pages();
-		
-		$data['courses'] = $this->courses_model->course_type($slug);
-		
-		$slug = ucwords($slug);
-		
-		$data['title'] = $slug;
-		
-	 
-
-
-	$this->load->view('templates/header', $data);
-	
-	$this->load->view('templates/nav-bar', $data);
-	
-	$this->load->view('courses/index', $data);
-	$this->load->view('templates/footer');
-	}
-	
-	
-	
-		/////////////////////////////////////////////////////////////////////////// Catagory Name
-	
-		public function cat($slug)
-	{
-		
-		
-			 $data['get_page_catagories'] = $this->pages_model->get_page_catagories();
-	 
-	 
-	 	$data['page_list'] = $this->pages_model->get_pages();
-		
-		
-	$data['cat_id'] = $this->courses_model->cat_id($slug);
-	
-	$data['cat_id'] = $data['cat_id']['id'];
-	
-	
-	
-	$data['courses'] = $this->courses_model->catagory($data['cat_id']);
-	
-	
-	//$slug ucwords($slug);
-	
-	$slug = strtoupper($slug);
-	
-	$slug = str_replace("-"," ",$slug );
-		
-	$data['page_name'] = $slug;
-		
-
-	$this->load->view('templates/header', $data);
-	
-	 $this->load->view('templates/nav-bar', $data);
-	
-	
-	$this->load->view('courses/catagory', $data);
-	$this->load->view('templates/footer');
-	
-	}
-	
-	
-			/////////////////////////////////////////////////////////////////////////////////////////////////////////
-	
-
-	
-		public function course($slug)
-	{
-	
-		$data['course'] = $this->courses_model->course($slug);
-		
-			 $data['get_page_catagories'] = $this->pages_model->get_page_catagories();
-	 
-	 
-	 	$data['page_list'] = $this->pages_model->get_pages();
-		
-		
-		
-
-	if (empty($data['course']))
-	{
-		show_404();
-	}
-
-	$data['title'] = $data['course']['title'];
-	
-	$data['url'] = $data['course']['url'];
-	
-	$data['study_mode'] = $data['course']['study_mode'];
-	$data['description'] = $data['course']['description'];
-	$data['who'] = $data['course']['who'];
-	$data['entry_requirements'] = $data['course']['entry_requirements'];
-	$data['what_study'] = $data['course']['what_study'];
-	$data['assessed'] = $data['course']['assessed'];
-	$data['where'] = $data['course']['where'];
-		
-
-	$this->load->view('templates/header', $data);
-	
-	$this->load->view('templates/nav-bar', $data);
-	
-	$this->load->view('courses/course', $data);
-	$this->load->view('templates/footer');
-	
-	
-	
-	}
-	
-	
-			/////////////////////////////////////////////////////////////////////////////////////////////////////////
-	
-		public function apply($page = 'home')
+class courses extends CI_Controller
 {
-	
-	
-	$this->load->helper('url');
-			
-	/* 
-	if ( ! file_exists('application/views/pages/'.$page.'.php'))
-	{
-		// Whoops, we don't have a page for that!
-		show_404();
-	}  
-	
-	 */
-	
-	$data['title'] = ucfirst($page); // Capitalize the first letter
-	
+    
+    
+    
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('courses_model');
+        
+        $this->load->model('pages_model');
+        
+        $this->load->helper('url');
+    }
+    
+    public function index()
+    {
+        $data['courses'] = $this->courses_model->course_type();
+        
+        
+        
+        $data['title'] = 'Full Course Archive';
+        
+        
+        $this->load->view('templates/header', $data);
+        
+        $this->load->view('templates/nav-bar', $data);
+        
+        
+        $this->load->view('courses/index', $data);
+        $this->load->view('templates/footer');
+        
+    }
+    
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    
+    public function course_type($slug) /////////////////// Young / Adult / Higher / International
+    {
+        
+        $data['get_page_catagories'] = $this->pages_model->get_page_catagories();
+        
+        
+        $data['page_list'] = $this->pages_model->get_pages();
+        
+        $data['courses'] = $this->courses_model->course_type($slug);
+        
+        $slug = ucwords($slug);
+        
+        $data['title'] = $slug;
+        
+        
+        
+        
+        $this->load->view('templates/header', $data);
+        
+        $this->load->view('templates/nav-bar', $data);
+        
+        $this->load->view('courses/index', $data);
+        $this->load->view('templates/footer');
+    }
+    
+    
+    
+    /////////////////////////////////////////////////////////////////////////// Catagory Name
+    
+    public function cat($slug)
+    {
+        
+        
+        $data['get_page_catagories'] = $this->pages_model->get_page_catagories();
+        
+        
+        $data['page_list'] = $this->pages_model->get_pages();
+        
+        
+        if ($data['cat_id'] = $this->courses_model->cat_id($slug)) {
+            
+            
+            
+            $data['cat_id'] = $data['cat_id']['id'];
+            
+            
+            
+            $data['courses'] = $this->courses_model->catagory($data['cat_id']);
+            
+            
+            $slug = strtoupper($slug);
+            
+            $slug = str_replace("-", " ", $slug);
+            
+            $data['page_name'] = $slug;
+            
+            
+            $this->load->view('templates/header', $data);
+            
+            $this->load->view('templates/nav-bar', $data);
+            
+            
+            $this->load->view('courses/catagory', $data);
+            $this->load->view('templates/footer');
+        } 
+        
+        else {
+            show_404();
+        }
+        
+    }
+    
+    
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    
+    
+    public function course($slug)
+    {
+        
+        $data['course'] = $this->courses_model->course($slug);
+        
+        $data['get_page_catagories'] = $this->pages_model->get_page_catagories();
+        
+        $data['page_list'] = $this->pages_model->get_pages();
 		
-		
-		$data['get_page_catagories'] = $this->pages_model->get_page_catagories();
-		
-		$data['page_list'] = $this->pages_model->get_pages();
-	
-	
-	
-	$this->load->view('apply/header');
-	
-	 $this->load->view('templates/nav-bar', $data);
-	
-	
-	$this->load->view('apply/index' , $data);
-	$this->load->view('apply/footer');
 
-}
+        if (empty($data['course'])) {
+            show_404();
+        }
+        
+        $data['title'] = $data['course']['title'];
+        
+        $data['url'] = $data['course']['url'];
+        
+        $data['study_mode']         = $data['course']['study_mode'];
+        $data['description']        = $data['course']['description'];
+        $data['who']                = $data['course']['who'];
+        $data['entry_requirements'] = $data['course']['entry_requirements'];
+        $data['what_study']         = $data['course']['what_study'];
+        $data['assessed']           = $data['course']['assessed'];
+        $data['where']              = $data['course']['where'];
+        
+        
+        $this->load->view('templates/header', $data);
+        
+        $this->load->view('templates/nav-bar', $data);
+        
+        $this->load->view('courses/course', $data);
+        $this->load->view('templates/footer');
+  
+    }
+    
+    
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    public function apply($page = 'home')
+    {
+        
+        
+        $this->load->helper('url');
+ 
+        $data['title'] = ucfirst($page); // Capitalize the first letter
+    
+        $data['get_page_catagories'] = $this->pages_model->get_page_catagories();
+        
+        $data['page_list'] = $this->pages_model->get_pages();
+    
+        
+        $this->load->view('apply/header');
+        
+        $this->load->view('templates/nav-bar', $data);
+        
+        
+        $this->load->view('apply/index', $data);
+        $this->load->view('apply/footer');
+        
+    }
+    
+    
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    
+    function course_search()
+    {
+          
+        $input = $_POST['search'];
+        
+        //filtering input for xss and sql injection
+        $input = strip_tags($input);
+        $input = mysql_real_escape_string($input);
+        $input = trim($input);
+        
 
-
-			/////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-  function course_search()
-        {
-			
-			
-
-$input =  $_POST['search'];
-
-//filtering input for xss and sql injection
-$input = strip_tags( $input );
-$input = mysql_real_escape_string( $input );
-$input = trim( $input );
-			
-			
-	 
-    	$data['search'] = $input;
-		
-		
-		if ($input == "" or $input =="enter a course search here") {
-			
-			
-			
-			
-			
-			$data['result'] = $this->load->view('courses/no_result', NULL, TRUE);
-			
-			$data['title'] = 'Search';
-			
-			
-			$this->load->view('templates/header', $data);
-		 
-		$this->load->view('courses/search', $data);
-	 
-	 	$this->load->view('templates/footer', $data);
-			
-			
-			
-			
-		} else {$data['result'] = "Your search term <Strong>".$input."</Strong> gave the following results: ";
-		
-		
-		
-
-    	$data['query']=$this->courses_model->search($this->input->post('search'));
-		
-		$data['title'] = 'Search';
-	 
-	    $this->load->view('templates/header', $data);
-		
-			$data['result'] = $this->load->view('courses/search-table', NULL, TRUE);
-		 
-		$this->load->view('courses/search', $data);
-		
-		
-	 
-	 	$this->load->view('templates/footer', $data);
-	
-		 }
-	
-		
-		
-		}
-	
-
+        $data['search'] = $input;
+        
+        
+        if ($input == "" or $input == "enter a course search here") {
+            
+            $data['result'] = $this->load->view('courses/no_result', NULL, TRUE);
+            
+            $data['title'] = 'Search';
+            
+            
+            $this->load->view('templates/header', $data);
+            
+            $this->load->view('courses/search', $data);
+            
+            $this->load->view('templates/footer', $data);
+        
+            
+        }
+        else {
+            $data['result'] = "Your search term <Strong>" . $input . "</Strong> gave the following results: ";
+   
+            $data['query'] = $this->courses_model->search($this->input->post('search'));
+            
+            $data['title'] = 'Search';
+            
+            $this->load->view('templates/header', $data);
+            
+            $data['result'] = $this->load->view('courses/search-table', NULL, TRUE);
+            
+            $this->load->view('courses/search', $data);
+   
+            $this->load->view('templates/footer', $data);
+            
+        }
+ 
+    }
+     
 }
 
 
--- application/views/admin/templates/header.php
+++ application/views/admin/templates/header.php
@@ -1,34 +1,32 @@
 <!DOCTYPE html>
-
 <!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
-<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
-<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
-<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
-<head>
-  <meta charset="utf-8" />
-
-  <!-- Set the viewport width to device width for mobile -->
-  <meta name="viewport" content="width=device-width" />
-
-  <title>Waltham Forest Admin</title>
-
-  <!-- Included CSS Files (Uncompressed) -->
-  <!--
-  <link rel="stylesheet" href="stylesheets/foundation.css">
-  -->
-  
-  <!-- Included CSS Files (Compressed) -->
-  <link rel="stylesheet" href="<?PHP echo base_url(); ?>administration/stylesheets/foundation.min.css">
-  <link rel="stylesheet" href="<?PHP echo base_url(); ?>administration/stylesheets/app.css">
-
-  <script src="<?PHP echo base_url(); ?>administration/javascripts/modernizr.foundation.js"></script>
-
-  <script src="<?PHP echo base_url(); ?>administration/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
-   	
-    
-        
-  
-         
-   
-    </head>
+<!--[if lt IE 7]> 
+<html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en">
+   <![endif]-->
+   <!--[if IE 7]>    
+   <html class="no-js lt-ie9 lt-ie8" lang="en">
+      <![endif]-->
+      <!--[if IE 8]>    
+      <html class="no-js lt-ie9" lang="en">
+         <![endif]-->
+         <!--[if gt IE 8]><!--> 
+         <html class="no-js" lang="en">
+            <!--<![endif]-->
+            <head>
+               <meta name="description" content="Waltham Forrest College International Courses">
+               <meta name="keywords" content="Waltham Forrest College International Courses">
+               <meta name="author" content="Daniel Roberts">
+               <meta charset="utf-8" />
+               <!-- Set the viewport width to device width for mobile -->
+               <meta name="viewport" content="width=device-width" />
+               <title>Waltham Forest Admin</title>
+               <!-- Included CSS Files (Uncompressed) -->
+               <!--
+                  <link rel="stylesheet" href="stylesheets/foundation.css">
+                  -->
+               <!-- Included CSS Files (Compressed) -->
+               <link rel="stylesheet" href="<?PHP echo base_url(); ?>administration/stylesheets/foundation.min.css">
+               <link rel="stylesheet" href="<?PHP echo base_url(); ?>administration/stylesheets/app.css">
+               <script src="<?PHP echo base_url(); ?>administration/javascripts/modernizr.foundation.js"></script>
+               <script src="<?PHP echo base_url(); ?>administration/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+            </head>
